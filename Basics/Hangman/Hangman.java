f9adb8cf2962d5e9661cb81512d6e9c12d44e265
Iftekhar A Chowdhury <007ifte@gmail.com>
2019-02-08 17:14:17 -0500

    Hangman

---
+++ Basics/Hangman/Hangman.java
@@ -0,0 +1,17 @@
+public class Hangman {
+
+  public static void main(String[] args) {
+    // Your incredible code goes here...
+      
+      Game game = new Game("iftekhar");
+      Prompter prompter = new Prompter(game);
+      
+      while( game.getRemainingTries() > 0){
+      prompter.displayProgress();
+      prompter.promptForGuess();
+      
+      }
+    
+      
+  }
+} 
